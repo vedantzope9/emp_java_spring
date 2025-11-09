bd5c69bb01ae08889581770fcd37db010ce5a3a1
Vedant Zope <vedantzope9@gmail.com>
2025-11-09 18:54:56 +0530

    Initial setup

---
+++ emp_java_spring/src/main/java/com/example/emp_java_spring/EmpJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.emp_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpJavaSpringApplication.class, args);
+	}
+
+}
---
+++ emp_java_spring/src/test/java/com/example/emp_java_spring/EmpJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emp_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
